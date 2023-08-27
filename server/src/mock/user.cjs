354a3853ce7281f1df0a936b4ad4bf75298f7c79
radomir-radionov <radomir.radionov26@gmail.com>
2023-08-28 01:49:44 +0300

    fix: client work correctly in docker

--- server/src/mock/user.cjs
+++ server/src/mock/user.cjs
@@ -4,6 +4,7 @@ module.exports = [
     lastName: 'Valadzko',
     email: 'psioozzz@tut.by',
     phone: '+375 (29) 512-32-60',
+    passwordHash: 'ddgsgdgdsgd11313',
     blocked: false,
     confirmed: true,
     provider: 'local',
