c7b6f6e553f0a36fda2273cad92c74bfe9bb45e2
Radomir <>
2023-12-11 00:28:20 +0300

    style

--- run-app.sh
+++ run-app.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd client yarn install && \
-cd ../server yarn install && \
+cd client && yarn install && \
+cd ../server && yarn install && \
 cp .env.example .env && \  
 yarn run db:migrate && \
 yarn run db:seed:up && \
-yarn run docker:rss && \
+yarn run docker:rs && \
 
 echo "Project installed and ready to run"
